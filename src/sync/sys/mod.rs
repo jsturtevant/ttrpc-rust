3cdeea0516c3d2b5fc49dc073ea0749eebde6c7f
James Sturtevant <jstur@microsoft.com>
2023-01-24 00:56:13 -0800

    Refactor to support other OSes
    This moves unix specific calls out of the main server and client
    functions.  It does this by introducing several new types: PipeListener, PipeConnection,  and ClientConnection.  These types are contain the unix specific functionality to communitcate with Unix Domain sockets and are hidden behind a conditional compilation flag.
    
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

---
+++ src/sync/sys/mod.rs
@@ -0,0 +1,9 @@
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub use crate::sync::sys::linux::{PipeConnection, PipeListener, ClientConnection};
+
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
+pub use crate::sync::sys::linux::net::{FD, LinuxListener, LinuxConnection};
