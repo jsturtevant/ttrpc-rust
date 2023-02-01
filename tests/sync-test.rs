d3a9f0c4f9c566a55108a0fdc5e317c1609927a9
James Sturtevant <jstur@microsoft.com>
2023-01-31 16:29:22 -0800

    fix up examples
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- tests/sync-test.rs
+++ tests/sync-test.rs
@@ -1,18 +1,13 @@
-use assert_cmd::prelude::*;
-use std::io::{stderr, stdout, Write};
+//use std::io::{stderr, stdout, Write};
 use std::{process::Command, time::Duration};
 
 #[test]
 fn run_sync_example() -> Result<(), Box<dyn std::error::Error>> {
-    build_example("server");
-    build_example("client");
-
     // start the server and give it a moment to start.
-    let mut cmd = Command::cargo_bin("examples/server")?;
-    let mut server = cmd.spawn()?;
+    let server = run_example("server").spawn();
     std::thread::sleep(Duration::from_secs(2));
 
-    let client = Command::cargo_bin("examples/client").unwrap().spawn();
+    let client = run_example("client").spawn();
     let mut client_succeeded = false;
     match client.unwrap().wait() {
         Ok(status) => {
@@ -24,26 +19,18 @@ fn run_sync_example() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // be sure to clean up the server, the client should have run to completion
-    server.kill()?;
+    server.unwrap().kill()?;
     assert!(client_succeeded);
     Ok(())
 }
 
-fn build_example(example: &str) {
+fn run_example(example: &str) -> Command {
     let mut cmd = Command::new("cargo");
-    let output = cmd
-        .arg("build")
+    cmd.arg("run")
         .arg("--example")
         .arg(example)
         .stdout(std::process::Stdio::piped())
         .stderr(std::process::Stdio::piped())
-        .current_dir("example")
-        .output()
-        .expect("failed to execute process");
-    let code = output.status.code().expect("should have status code");
-    if code != 0 {
-        stdout().write_all(&output.stdout).unwrap();
-        stderr().write_all(&output.stderr).unwrap();
-        panic!("failed to command cargo build for {}", example);
-    }
+        .current_dir("example");
+    cmd
 }
