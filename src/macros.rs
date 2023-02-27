7b11467525f3dbe444a69214567c5a0cf1c5fb3f
James Sturtevant <jstur@microsoft.com>
2023-02-27 13:27:29 -0800

    fix macos
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- src/macros.rs
+++ src/macros.rs
@@ -18,7 +18,7 @@ macro_rules! cfg_sync {
 macro_rules! cfg_async {
     ($($item:item)*) => {
         $(
-            #[cfg(all(feature = "async", target_os="linux"))]
+            #[cfg(all(feature = "async", target_family="unix"))]
             #[cfg_attr(docsrs, doc(cfg(feature = "async")))]
             $item
         )*
